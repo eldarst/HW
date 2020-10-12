8e0fb8634a87fd356e279c23ccf945ded4ab92e0
Eldar Zhaanbaev <eldar.zhaanbaev@gmail.com>
2020-10-12 18:27:58 +0300

    2.0

---
+++ src/main/cash_count/cash/Cash.java
@@ -0,0 +1,81 @@
+package cash_count.cash;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class Cash {
+    private long sum;
+    private HashSet<Long> allNominals;
+    private HashSet<HashSet<Long>> nominalComb = new HashSet<>();
+    private HashSet<HashMap<Long, Integer>> linearComb = new HashSet<>();
+
+    public Cash(Long c, final HashSet<Long> nominal1) {
+        this.sum = c;
+        this.allNominals = nominal1;
+    }
+
+
+    public final void printAllComb() {
+        System.out.println("Количество комбинаций: " + linearComb.size());
+        for (HashMap<Long, Integer> lin : linearComb) {
+            boolean t = true;
+            for (Map.Entry<Long, Integer> entry : lin.entrySet()) {
+                if (t) {
+                    System.out.print(entry.getKey() + "*" + entry.getValue());
+                    t = false;
+                } else {
+                    System.out.print(" + " + entry.getKey() + "*" + entry.getValue());
+                }
+            }
+            System.out.println("=" + sum);
+        }
+    }
+
+    //Возвращает все добавленные линейные комбинации
+    public HashSet<HashMap<Long, Integer>> getLinearComb() {
+        return linearComb;
+    }
+
+    //Добавляем новую комбинацию номиналов
+    public void addNominalComb(final HashSet<Long> comb) {
+        nominalComb.add(comb);
+    }
+
+    //Купюры для размена
+    public HashSet<Long> getAllNominals() {
+        return allNominals;
+    }
+
+    public long getSum() {
+        return sum;
+    }
+
+    //Проверяем была ли добавлена комбинация в список удовлетворяющих номиналов
+    public boolean checkNominalComb(final HashSet<Long> comb) {
+        if(!nominalComb.isEmpty()) {
+            return false;
+        }
+        else if (nominalComb.contains(comb)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //Добавляем линейные комбинацию номиналов: [a1 * x1], [a1 *x2, a2 * x3] ...
+    // x1,x2,x3... коэффициенты
+    public void addLinearComb(final HashMap<Long, Integer> HashMap1) {
+        linearComb.add(HashMap1);
+    }
+
+    // Проверяем была ли добавлена комбинация в спискок удовлетворяющих лин.комб.
+    public boolean checkLinearComb(HashMap<Long, Integer> HashMap1) {
+        if (linearComb.contains(HashMap1)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
---
+++ src/test/MainTest.java
@@ -0,0 +1,43 @@
+import cash_count.main.Main;
+import cash_count.cash.Cash;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+
+public class MainTest {
+    @Test
+    public void InputTest() {
+        
+    }
+    @Test
+    public void TestMain() {
+        long sum = 5;
+        HashSet<Long> nominals = new HashSet<>();
+        // nominals = [1, 2]
+        for (long i = 1; i < 3; ++i) {
+            nominals.add(i);
+        }
+        Cash cash = new Cash(sum, nominals);
+        HashSet<Long> empty = new HashSet<>();
+        Main.findNominalComb(cash, nominals.size(), empty);
+        int result = cash.getLinearComb().size();
+        Assertions.assertEquals(3, result);
+        HashSet<HashMap<Long, Integer>> expectedLinearComb = new HashSet<>();
+        HashMap<Long, Integer> linearCombination1 = new HashMap<>();
+        linearCombination1.put((long)1, 5);
+        expectedLinearComb.add(linearCombination1);
+        HashMap<Long, Integer> linearCombination2 = new HashMap<>();
+        linearCombination2.put((long)1,1);
+        linearCombination2.put((long)2,2);
+        expectedLinearComb.add(linearCombination2);
+        HashMap<Long, Integer> linearCombination3 = new HashMap<>();
+        linearCombination3.put((long)1,3);
+        linearCombination3.put((long)2,1);
+        expectedLinearComb.add(linearCombination3);
+        Assertions.assertEquals(expectedLinearComb, cash.getLinearComb());
+    }
+}
